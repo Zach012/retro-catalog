9750b4e5ca28dd09fd336817c51e5782b173c98e
zach <zachdillon012@gmail.com>
2021-06-07 22:35:19 -0400

    intitial commit

---
+++ src/main/java/com/example/RetroCatalog/RetroCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetroCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RetroCatalog/RetroCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetroCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
